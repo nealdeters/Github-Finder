e481f50b788ffa6063e5b110aa7ed0fc87f6906b
Neal Deters <nealdeters@gmail.com>
2020-04-22 14:48:10 -0500

    created general application and users/useritem components for listing out github user info

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,19 @@
+import React, {Fragment} from 'react'
+import spinner from './spinner.gif'
+
+const Spinner = props => {
+	return (
+		<Fragment>
+			<img 
+				src={spinner} 
+				alt="Loading..." 
+				style={{width: '200px', margin: 'auto', display: 'block'}}/>
+		</Fragment>
+	)
+}
+
+Spinner.propTypes = {
+
+}
+
+export default Spinner
