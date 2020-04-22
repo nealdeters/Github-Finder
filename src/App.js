1af85f6eaf05466dc10a08cfb65f9ac8186118c4
Neal Deters <nealdeters@gmail.com>
2020-04-22 17:48:38 -0500

    added user route and user info page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import User from './components/users/User'
 import Search from './components/users/Search'
+import Alert from './components/layout/Alert'
+import About from './components/pages/About'
 import axios from 'axios'
 
 import './App.css';
@@ -9,7 +13,10 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    user: {},
+    repos: [],
+    loading: false,
+    alert: null
   }
 
   // async componentDidMount() {
@@ -18,37 +25,89 @@ class App extends Component {
   //   this.setState({users: res.data, loading: false});
   // }
 
+  // set alert
+  setAlert = (msg, type) => {
+    this.setState({alert: {msg, type}});
+    setTimeout(() => {
+      this.setState({alert: null});
+    }, 3000)
+  }
+
+  // search github users
+  searchUsers = async (text) => {
+    this.setState({loading: true});
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({users: res.data.items, loading: false});
+  }
+
+  // get single github user
+  getUser = async (username) => {
+    this.setState({loading: true});
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    console.log(res.data)
+    this.setState({user: res.data, loading: false});
+  }
+
+  // get user repos
+  getUserRepos = async (username) => {
+    this.setState({loading: true});
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    console.log(res.data)
+    this.setState({repos: res.data, loading: false});
+  }
+
   // clear users from state
   clearUsers = () => {
     this.setState({users: [], loading: false});
   }
 
-  // search github users
-  searchUsers = async (text) => {
-    if(text !== ''){
-      this.setState({loading: true});
-      const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-      this.setState({users: res.data.items, loading: false});
-    }
-  }
-
   render() {
-    const {users, loading} = this.state;
+    const {repos, user, users, loading, alert} = this.state;
 
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Search 
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false} />
-          <Users 
-            loading={loading}
-            users={users}
-            />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={alert} />
+
+            <Switch>
+              <Route 
+                exact 
+                path="/" 
+                render={props => (
+                <Fragment>
+                  <Search 
+                    searchUsers={this.searchUsers}
+                    clearUsers={this.clearUsers}
+                    showClear={users.length > 0 ? true : false}
+                    setAlert={this.setAlert} />
+                  <Users 
+                    loading={loading}
+                    users={users}
+                    />
+                </Fragment>
+              )} />
+              <Route 
+                exact 
+                path="/about" 
+                component={About}/>
+              <Route 
+                exact 
+                path="/user/:login"
+                render={props => (
+                  <User 
+                    {...props} 
+                    getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
+                    user={user}
+                    repos={repos}
+                    loading={loading} />
+                )} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom' 
 import PropTypes from 'prop-types'
 
 const UserItem = ({ user: {login, avatar_url, html_url} }) => {
 	return (
 		<div className="card text-center">
-			<img src={avatar_url} alt="" className="round-img" style={{width: '60px'}} />
+			<img 
+				src={avatar_url} 
+				alt="" 
+				className="round-img" 
+				style={{width: '60px'}} />
 			<h3>{login}</h3>
 			<div>
-				<a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+				<Link 
+					to={`/user/${login}`} 
+					className="btn btn-dark btn-sm my-1">More</Link>
 			</div>
 		</div>
 	)
